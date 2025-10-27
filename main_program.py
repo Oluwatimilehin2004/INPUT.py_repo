246514792ca278b660469386936dd3f22a3649a8
Oluwatimilehin2004 <oluwatimilehinayo2004@gmail.com>
2025-10-27 17:05:44 -0500

    final debug

--- main_program.py
+++ main_program.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from helper_functions import validate_input, convert_to_binary, create_message
 from file_manager import save_message, read_message
 from greetings import show_intro, show_exit_message
@@ -16,9 +15,8 @@ def get_user_info():
             continue
 
         return name, age
-=======
-<<<<<<< HEAD
-if _name_ == "_main_":
+
+if __name__ == "__main__":
     # Show intro
     show_intro()
 
@@ -42,26 +40,3 @@ if _name_ == "_main_":
 
     # Show exit message
     show_exit_message()
-
-
-=======
-def save_message(message):
-    try:
-        with open("user_message.txt", "w") as file:
-            file.write(message)
-        print("Message saved successfully.")
-    except Exception as e:
-        print(f"Error saving message: {e}")
-
-def read_message():
-    try:
-        print("Reading saved message...")
-        with open("user_message.txt", "r") as file:
-            content = file.read()
-            print(content)
-    except FileNotFoundError:
-        print("No saved message found.")
-    except Exception as e:
-        print(f"Error reading message: {e}")
->>>>>>> 5d4da82bbf73af157750740f66b1945592416ca2
->>>>>>> 875900d8894e52eff9e0b2c80376ab59ce4f0a3f
