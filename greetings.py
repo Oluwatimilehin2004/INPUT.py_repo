8482f56fb6095620e95b80a41372175bfa103eaa
Vanessa-Ace <v.nishimwe@alustudent.com>
2025-10-27 15:02:06 -0700

    Greetings and Goodbyes

---
+++ greetings.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def show_intro():
+    print("=============================================")
+    print("Welcome to the User Info Program")
+
+    print("=============================================\n")
+
+
+def show_exit_message():
+    print("\nThank you for using the program.Goodbye!")
+
+
